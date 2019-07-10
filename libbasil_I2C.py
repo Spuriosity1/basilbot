18af313c4232338b9073c64020fb6a6a06189de3
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-10 14:57:45 +1000

    repair dodgy type tomfoolery

--- libbasil_I2C.py
+++ libbasil_I2C.py
@@ -15,14 +15,14 @@ bus = smbus2.SMBus(1)
 
 
 def water(speed, time):
-    req_str  = struct.pack('<B',speed)
-    req_str += struct.pack('<H',1024*time)
+    req_str  = struct.pack('>B',speed)
+    req_str += struct.pack('>H',1024*time)
     bus.write_i2c_block_data(CHANNEL, ord('S'), req_str)
 
 def moisture_read():
     # read a block of 4 bytes (offset 0)
     res = bus.read_i2c_block_data(CHANNEL, 0, 4)
-    return struct.unpack('<HBB',res)
+    return struct.unpack('>HBB',res)
 
 
 def sample_data(N):
