e7b362fca7dd049c4493b213e92e433445b26939
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-10 14:07:09 +1000

    Fix up messy libbasil_I2C code

--- libbasil_I2C.py
+++ libbasil_I2C.py
@@ -16,8 +16,8 @@ bus = smbus2.SMBus(1)
 
 def water(speed, time):
     req_str  = struct.pack('<B',speed)
-    req_str += struct.pack('<H',1000*time)
-    bus.write_i2c_block_data(CHANNEL, 'S', req_str)
+    req_str += struct.pack('>H',1000*time)
+    bus.write_i2c_block_data(CHANNEL, ord('S'), req_str)
 
 def moisture_read():
     # read a block of 4 bytes (offset 0)
