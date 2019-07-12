5e581818894c5d07f290ec9c650589c1003e8ec6
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-12 18:02:00 +1000

    that's not how data works

--- saveStats.py
+++ saveStats.py
@@ -6,7 +6,7 @@ from libbasil_I2C import sample_data
 
 with open("../data/history.csv",'a') as f:
     writer = csv.writer(f)
-    writer.writerow(sample_data(15))
+    writer.writerow([time.strftime('%Y-%m-%dT%H:%M:%S'), sample_data(15)])
 
 
 # a tiny tiny script that does not a lot
