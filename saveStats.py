a1c12d585b1fdd81a274834de09459341e2fd11e
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-12 18:09:16 +1000

    Parameter tweaks

--- saveStats.py
+++ saveStats.py
@@ -7,7 +7,7 @@ from libbasil_I2C import sample_data
 
 with open("../data/history.csv",'a') as f:
     writer = csv.writer(f)
-    writer.writerow([time.strftime('%Y-%m-%dT%H:%M:%S'), sample_data(15)])
+    writer.writerow([time.strftime('%Y-%m-%dT%H:%M:%S'), sample_data(5)])
 
 
 # a tiny tiny script that does not a lot
