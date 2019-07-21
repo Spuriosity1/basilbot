96c705c4b8da3e5e0f247401ded22f8aad4947a9
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-21 14:42:42 +1000

    Remove webhook from GitHub

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading
 import time
 import os
 
-from libbasil import *
+from libbasil_I2C import *
 
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
@@ -13,14 +13,10 @@ DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 HELPTEXT='''Commands:
 (basil pay respects)
 (basil play [SONG])
-!basil moisture
-!basil history
 !basil help
 !basil flip the table
 !basil chuck a yeet
 '''
-
-
 
 client=discord.Client()
 
@@ -43,53 +39,6 @@ async def on_message(message):
             stderr=subprocess.STDOUT)
 
         msg = 'Now playing: Despacito ft. Daddy Yankee'
-        await message.channel.send(msg)
-
-    elif content.startswith('!basil') or content.startswith('basil,'):
-        cmd = content[7:]
-        msg = HELPTEXT
-
-        if cmd.startswith('moisture'):
-            msg = "Soil moisture content: {:.1f}%\n".format(getMoisture())
-
-        elif cmd.startswith('history'):
-            num = 12
-            try:
-                num = int(message.content[len('!basil history'):])
-            except ValueError:
-                pass
-            msg = getHistory(num)
-
-
-        elif cmd.startswith('water'):
-            # NOTE: This needs a heuristic model of soil moisture
-            tau = 5
-            try:
-                tau = int(message.content[len('!basil water'):])
-            except ValueError:
-                tau=5
-
-            if tau > 60:
-                msg= 'You may not water for more than 1 minute.'
-            elif tau <0:
-                msg= "Nice try, you won't fool me with that one again"
-            else:
-                moisture = getMoisture()
-                if moisture > 70:
-                    msg = 'You should not water me any more, the soil is '
-                    msg += 'already {:.1f}% damp'.format(moisture)
-                else:
-                    pumpPulse(tau)
-                    msg = "{} seconds of S i p p\n".format(tau)
-
-        elif cmd.startswith('please run the motherfucking pump'):
-            # the super secret override message. Don't tell anyone.
-            pumpPulse(1)
-            msg = "{} seconds of S i p p\n".format(tau)
-        elif cmd.startswith('chuck a yeet'):
-            msg='(╯°□°）╯︵ YEET'
-        elif cmd.startswith('flip the table'):
-            msg='(╯°□°）╯︵ ┻━┻'
         await message.channel.send(msg)
 
 @client.event
